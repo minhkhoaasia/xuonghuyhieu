c81102fd1ea644f03ff41593a2807f29ed08eebd
unknown <MinhkhoaAsia@.(none)>
2013-05-03 11:57:20 +0700

    Edit 1 chut ve Copyright Them trang chu

---
+++ Xuong-huy-hieu_edgeActions.js
@@ -0,0 +1,36 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTimelineAction(compId,symbolName,"Default Timeline","complete",function(sym,e){sym.play(0);});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_danh_cho_khach_hang}","mouseover",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_danh_cho_khach_hang}","mouseout",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_button1}","mouseover",function(sym,e){sym.$("danh_cho_khach_hang").css("border","8px solid #000000");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_button1}","mouseout",function(sym,e){sym.$("danh_cho_khach_hang").css("border","8px solid #ffffff");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_button1}","click",function(sym,e){window.open("http://www.xuonghuyhieu.com/danh-cho-khach-hang","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_button2}","mouseover",function(sym,e){sym.$("dat_hang").css("border","8px solid #000000");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_button2}","mouseout",function(sym,e){sym.$("dat_hang").css("border","8px solid #ffffff");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_button2}","click",function(sym,e){window.open("http://www.xuonghuyhieu.com/dat-hang","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Group6}","click",function(sym,e){window.open("http://www.xuonghuyhieu.com/lien-he","_self");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Group6}","mouseover",function(sym,e){sym.$("lien_he").css("border","8px solid #000000");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Group6}","mouseout",function(sym,e){sym.$("lien_he").css("border","8px solid #ffffff");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_button1}","mousedown",function(sym,e){sym.$("danh_cho_khach_hang").css("border","8px solid #787878");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_button2}","mousedown",function(sym,e){sym.$("dat_hang").css("border","8px solid #787878");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Group6}","mousedown",function(sym,e){sym.$("lien_he").css("border","8px solid #787878");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-335126214");
